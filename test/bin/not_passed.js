2b96206ab18e78af0958a4125f7c4bedbf8d5ddb
Yoshiya Hinosawa <stibium121@gmail.com>
2016-08-13 12:25:35 +0900

    debug

--- test/bin/not_passed.js
+++ test/bin/not_passed.js
@@ -6,6 +6,10 @@ const pass = './test/agreed.json5';
 const not_pass = './test/not_passed.json5';
 plzPort().then((port) => {
   const proc = cp.exec(`node ${process.cwd()}/bin/agreed-server.js --port ${port} --path ${not_pass}`);
+
+  proc.on('error', err => {
+    console.log(err.stack)
+  })
   setTimeout(() => {
     const result = cp.execSync(`node ${process.cwd()}/bin/agreed-client.js --port ${port} --path ${pass}`).toString();
 
